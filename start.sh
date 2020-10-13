39b2202d5818693cd3b3141369de9852b79d2cc7
9Pi <9Pi>
2020-10-13 20:01:53 +0100

    Better aproach to start services in docker container

--- start.sh
+++ start.sh
@@ -2,40 +2,17 @@
 
 echo "Hello from container!"
 
-# Start Mopidy
-mopidy > /root/.local/mopidy/mopidy-log.txt 2>&1 &
-
-status=$?
-
-if [ $status -ne 0 ]; then
-        echo "Failed to start mopidy..."
-	exit
-else
-	echo "Mopidy started successfully!"
-fi
-
-# Start Snapserver
-
 snapserver -d
+
 status=$?
 
 if [ $status -ne 0 ]; then
-	echo "Failed to start Snapserver.."
+        echo "Failed to start snapserver..."
 	exit
 else
 	echo "Snapserver started successfully!"
 fi
 
-# Check every 60 sec if snapserver failed
-while sleep 60; do
-        ps aux |grep snapserver |grep -q -v grep
-        status1=$?
+echo "Starting Mopidy!"
 
-        if [ $status1 -ne 0 ]; then
-                echo "Snapserver failed"
-                exit 1
-        fi
-done
-
-
-
+mopidy
