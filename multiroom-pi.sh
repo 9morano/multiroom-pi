51013060d8d28d3b221ce61c721bfa4e411cac58
9Pi <9Pi>
2020-10-13 12:11:21 +0100

    Updated start script

--- multiroom-pi.sh
+++ multiroom-pi.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-
+echo "Starting container"
 # Start the mopidy and snapcast container
 docker run --name=multiroom-pi -d --net=host -v "$(pwd)/Music:/root/Mopidy/Music" -v "$(pwd)/data:/root/.local/mopidy" mopidy-snapcast
 
 # Sleep for few seconds so container can run
-#sleep(5)
+#sleep 5
 
+# Obtain IP address of the Raspberry-Pi
+IPADDR=$(hostname -I)
+
+echo "Starting client - connecting to address ${IPADDR:0:13}"
 # Start the snapclient in background and store its output
-snapclient -h 192.168.2.180 > ~/multiroom-pi/data/snapclient-log.txt 2>&1 &
+snapclient -h ${IPADDR:0:13} > ~/multiroom-pi/data/snapclient-log.txt 2>&1 &
