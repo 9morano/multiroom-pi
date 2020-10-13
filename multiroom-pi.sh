6415450f4bfb5a15b0cc6711497c13dea36262c2
9Pi <9Pi>
2020-10-13 14:00:53 +0100

    Updated start script

--- multiroom-pi.sh
+++ multiroom-pi.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "Starting container"
-# Start the mopidy and snapcast container
+# Start the container with Mopidy and Snapserver running in it
+# And run the snapclient to get the music from the server
+#
+# To exit run:
+# $ docker kill multiroom-pi && docker rm multiroom-pi
+# $ kill $(pidof snapclient)
+
+echo "Starting Mopidy and Snapserver container (container name = multiroom_pi)"
 docker run --name=multiroom-pi -d --net=host -v "$(pwd)/Music:/root/Mopidy/Music" -v "$(pwd)/data:/root/.local/mopidy" mopidy-snapcast
 
-# Sleep for few seconds so container can run
-#sleep 5
+# Set the audio volume to the max (max = 400)
+echo "Set RPi volume to 100%" 
+amixer cset numid=1 -- 400
 
 # Obtain IP address of the Raspberry-Pi
 IPADDR=$(hostname -I)
